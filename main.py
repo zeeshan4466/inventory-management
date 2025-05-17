455a6e138d7bf467ba7f12a7d7a5cb1702c57413
zeeshan4466 <zeeshan.zulfiqar11@gmail.com>
2025-05-17 14:42:56 +0500

    database connection successful

---
+++ main.py
@@ -0,0 +1,25 @@
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+import mysql.connector
+import os
+from mysql.connector import Error
+
+app = FastAPI()
+
+
+
+try:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ecommerce_db"
+    )
+    cursor = db.cursor(dictionary=True)
+    print("Connection successfull",cursor)
+except Error as e:
+    print("Connection not successfull")
+
