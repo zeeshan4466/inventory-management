5f21663dd54f2e0757de1db371d660fe7b8a10c2
zeeshan4466 <zeeshan.zulfiqar11@gmail.com>
2025-05-18 11:12:39 +0500

    Create sales.py

---
+++ model/sales.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+class Sales(BaseModel):
+    start_date: Optional[date]
+    end_date: Optional[date]
+    product_id: Optional[int]
+    category: Optional[str]
